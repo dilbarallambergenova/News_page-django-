91ac69404ca72b0fac869f7e7afa0d1942f71713
dilbarallambergenova <allambergenovadilbar9@gmail.com>
2025-09-30 15:52:51 +0500

    updated

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG",default=False)
-ALLOWED_HOSTS = ["157.230.157.27", "127.0.0.1", "localhost"]
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[])
+
 
 
 
